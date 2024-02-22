31357c98cf2ed9ebe8fab96e32f63d68886dd128
PoohJr <Williamdomenech06@gmail.com>
2024-02-22 07:46:53 -0500

    lil update

--- PokeApi/src/massEffect.jsx
+++ PokeApi/src/massEffect.jsx
@@ -25,13 +25,11 @@ import React, {useState, useEffect} from "react";
     function massEffect(){
 
         const [count, setCount] = useState(0)
+        const [color, setColor] = useState("Green")
 
         useEffect(() =>{
-            document.title = `"Count" ${count}`
-            //  document.title = `"Count" ${count}` this would change the title on the site everytime its pressed
-            //document.title = `"Count" ${count}` if you only wanna update things once
-            //you put an empty array after the blue {}[] 
-        })
+            document.title = `"Count" ${count} ${color}`
+        }, [count, color])
 
         function addCount(){
 
@@ -39,11 +37,46 @@ import React, {useState, useEffect} from "react";
             
         }
 
+        function minusCount(){
+
+            setCount(c =>c - 1)
+            
+        }
+
+        function changeColor(){
+            setColor(c => c === "green" ? "red" : "green")
+        }
+        
         return(<>
-            <p>Count {count}</p>
+            <p style={{color : color}}>Count {count}</p>
             <button onClick={addCount}>Add</button>
+            <button onClick={minusCount}>Subtract</button> <br></br>
+            <button onClick={changeColor}>Change Color</button>
         
         </>);
     }
 
 export default massEffect
+
+// useEffect(() =>{
+//     document.title = `"Count" ${count}`
+// }, [count])
+
+// What this is saying is if the Value in the array i chnaged then 
+//it could run wahts in the funcgtion
+// yes it works without it, but it more specific
+// it will run for whatever reason the componet re renders so and a value in the array
+
+// useEffect(() =>{
+//     document.title = `"Count" ${count}`
+// })
+
+ //  document.title = `"Count" ${count}` this would change the title on the site everytime its pressed
+
+
+    //document.title = `"Count" ${count}` if you only wanna update things once
+    //you put an empty array after the blue {}[] 
+
+    // useEffect(() =>{
+    //     document.title = "Count"
+    // }, [])
