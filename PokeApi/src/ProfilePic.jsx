475e91aad1f38277e700712d646adfcbfb4b4cd6
PoohJr <145505256+PoohJr@users.noreply.github.com>
2024-02-29 06:54:15 -0500

    Delete PokeApi directory

--- PokeApi/src/ProfilePic.jsx
+++
@@ -1,9 +0,0 @@
-
-function ProfilePic(){
-    const imgurl = './src/assets/naruro.png';
-    
-    const handleClick = (e) =>e.target.style.border = "2em black solid"
-   
-    return(<img onClick={(e) =>handleClick (e)} src={imgurl} alt="Profile Pic"></img>);
-}
-export default ProfilePic
--- PokeApi/src/UserGreeting.jsx
+++
@@ -1,8 +0,0 @@
-
-function UserGreeting(props){
-    return(
-        
-    );
-}
-
-export default UserGreeting
