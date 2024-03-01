757f03ffadfa4a4818d39e2de8c8db3471591d15
William Domenech <williamdomenech06@gmail.com>
2024-03-01 12:18:44 -0500

    got iy

--- Poke/src/Header.jsx
+++ Poke/src/Header.jsx
@@ -1,66 +1,65 @@
 import React, {useState, useEffect} from "react";
-import './header.css'
+import './Header.css'
 
-function Header(){
-    const [userinput, setUserInput] = useState("")
-    const [data, setData] = useState(null)
-    const [error, setError] = useState(null)
+    function Header(){
+        const [userInput, setUserInput] = useState("")
+        const [data, setData] = useState(null)
+        const [newerror, setNewError] = useState(null)
+        const apiurl = `https://pokeapi.co/api/v2/pokemon/${userInput}`
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Form submitted with value:", userinput);
-        setUserInput("")
-      };
-
-
-         useEffect(() => {
-        const fetchData = async () => {
-          try {
-            if(!userinput) return;
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${userinput}`);
-            console.log(response)
-            if(!response.ok){
-              throw new Error("Failed to Reach API")
-            }
-            const jsonData = await response.json();
-            setData(jsonData);
-          } catch (error) {
-            setError(console.error("Something went bad", error));
-          }
+        function HandleInputChange(e){
+            setUserInput(e.target.value)
         }
-      
-        fetchData();
-      }, [userinput]);
-      
-    function remove(){ 
-     
-}
 
-    function randomPoke(){
-        
-    }
+        const handleSubmit = (e) => {
+            e.preventDefault();
+            console.log("Form submitted with value:", userInput);
+            setUserInput("")
+            };
 
+
+            // useEffect(() => {
+            //     const fetchData = async () => {
+            //     try {
+            //         if(!userInput) return;
+            //         const response = await fetch(apiurl);
+            //         if(!response.ok){
+            //             throw new Error("Failed to Reach API")
+            //             }
+            //         const jsonData = await response.json();
+            //         setData(jsonData);
+                    
+            //         } catch (error) {
+            //         setNewError(console.error("Something went bad", newerror));
+            //         }
+            //     }
+            //     fetchData();
+            //     }, [data]);
+                const fetchData = async () => {
+                       const response = await fetch(apiurl);
+                       if(!response){
+                        return setNewError(console.error(newerror, "No Good Play Boi"))
+                       }
+                       const ResData = await response.json();
+                       console.log(ResData)
+
+                }
     return(
     <>
-    <div className="container">
-        <form onSubmit={handleSubmit}>
 
-            <h1 className="heading-text">PokeMon</h1>
-
-            <input id="in" onChange={(e) => setUserInput(e.target.value)}
-             value={userinput}
-             type="text"
-             placeholder="Choose Your Pokemon!"></input>
-            <br></br>
-
-            <button onClick={randomPoke}>Randomize</button>
-
+    <div className="container"> 
+        <form onSubmit={handleSubmit}> 
+            <h1 className="heading-text">PokeMon</h1> 
+                <input id="in" 
+                onChange={HandleInputChange} 
+                value={userInput} type="text" 
+                placeholder="Choose Your Pokemon!"
+                ></input> <button onClick={fetchData}>Search</button>
+                <br></br> 
+                <button>Randomize</button> 
         </form>
-    </div>
-        
-        
-    </>)
-}
+     </div>
 
+    </>)
+    }
 export default Header
