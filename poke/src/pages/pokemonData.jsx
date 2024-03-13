8c6a5f6a0e12849b9afa1c57ed914744e0128cbf
PoohJr <Williamdomenech06@gmail.com>
2024-03-13 07:43:51 -0400

    add tailwind
    addin more info i want also gonna try tailwindcss to make it more pleasing to the eye

--- poke/src/pages/pokemonData.jsx
+++ poke/src/pages/pokemonData.jsx
@@ -5,14 +5,25 @@ function PokemonData() {
     const location = useLocation();
     const pokedata = location.state ? location.state.pokedata : null;
     console.log(location.state.pokedata)
+    const Playaudio = () => { 
+        if(pokedata){
+        new Audio(`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokedata.id}.ogg`).play()
+        }
+    }
+
     return (
         <div>
             <h1>Pokemon Data</h1>
             {pokedata && (
                 <div>
                     <h2>Name: {pokedata.name}</h2>
+                    <p>Poke#: {pokedata.id}</p>
+                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokedata.id}.png`} alt="Pokemon Image"/>
                     <p>Height: {pokedata.height}</p>
                     <p>Weight: {pokedata.weight}</p>
+                    <button onClick={Playaudio}>PokeMon Cry </button> 
+                    <p>Where ya can find em : {pokedata.location_area_encounters}</p>
+
                     
                 </div>
             )}
