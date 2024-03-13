900ab904135e5a92b4d4a8cfd784ec5409ecc76c
PoohJr <Williamdomenech06@gmail.com>
2024-03-13 10:52:37 -0400

    maybe

--- poke/src/App.jsx
+++ poke/src/App.jsx
@@ -3,16 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PokemonData from "./pages/PokemonData.jsx"; // Corrected path
 import UserPoke from "./pages/UserPoke.jsx"; // Corrected path
 import NonLoad from './pages/NonLoad.jsx';
+import './index.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Header />} /> {/* Set the default route */}
-        <Route path="/pokemonData" element={<PokemonData />} /> {/* Corrected path */}
-        <Route path="/userPoke" element={<UserPoke />} /> {/* Corrected path */}
-        {/* Add other routes as needed */}
-        {/* <Route path="*" element={<NonLoad />} /> */}
+        <Route path="/" element={<Header />} /> 
+        <Route path="/pokemonData" element={<PokemonData />} /> 
+        <Route path="/userPoke" element={<UserPoke />} /> 
+        
+        { <Route path="*" element={<NonLoad />} /> }
       </Routes>
     </BrowserRouter>
   );
